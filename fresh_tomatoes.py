f81bfa8df0c93551ba562b325b3bd76591e8c1bb
Eduardo Mendivil <emendivil@sci.utah.edu>
2017-10-19 00:24:21 -0600

    added my files

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -2,11 +2,8 @@ import webbrowser
 import os
 import re
 
-
 # Styles and scripting for the page
 main_page_head = '''
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="utf-8">
     <title>Fresh Tomatoes!</title>
@@ -14,11 +11,19 @@ main_page_head = '''
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
     <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
     <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
     <style type="text/css" media="screen">
         body {
             padding-top: 80px;
+            background: -webkit-linear-gradient(0deg, #2424c5, #00b6ed); /* For Safari 5.1 to 6.0 */
+            background: -o-linear-gradient(0deg, #2424c5, #00b6ed); /* For Opera 11.1 to 12.0 */
+            background: -moz-linear-gradient(0deg, #2424c5, #00b6ed); /* For Firefox 3.6 to 15 */
+            background: linear-gradient(0deg, #2424c5, #00b6ed); /* Standard syntax */
+            font-family: 'Questrial', sans-serif;
+            color: white;
+            text-transform: uppercase;
         }
         #trailer .modal-dialog {
             margin-top: 200px;
@@ -40,7 +45,10 @@ main_page_head = '''
             padding-top: 20px;
         }
         .movie-tile:hover {
-            background-color: #EEE;
+            background: -webkit-linear-gradient(0deg, #2424c5, #04aaea); /* For Safari 5.1 to 6.0 */
+            background: -o-linear-gradient(0deg, #2424c5, #04aaea); /* For Opera 11.1 to 12.0 */
+            background: -moz-linear-gradient(0deg, #2424c5, #04aaea); /* For Firefox 3.6 to 15 */
+            background: linear-gradient(0deg, #2424c5, #04aaea); /* Standard syntax */
             cursor: pointer;
         }
         .scale-media {
@@ -55,6 +63,22 @@ main_page_head = '''
             left: 0;
             top: 0;
             background-color: white;
+        }
+        .navbar-inverse {
+            background-image: none;
+            background-repeat: none;
+            background-color: #42000b;
+            border-color: #f0ad4e;
+        }
+        .navbar-inverse .navbar-brand {
+            color: #efefef;
+            text-transform: uppercase;
+            font-weight: bolder;
+            font-size: 30px;
+        }
+        h2 {
+            font-size: 26px;
+            border-bottom: solid 5px #42000b;
         }
     </style>
     <script type="text/javascript" charset="utf-8">
@@ -85,9 +109,10 @@ main_page_head = '''
 </head>
 '''
 
-
 # The main page layout and title bar
 main_page_content = '''
+<!DOCTYPE html>
+<html lang="en">
   <body>
     <!-- Trailer Video Modal -->
     <div class="modal" id="trailer">
@@ -119,7 +144,6 @@ main_page_content = '''
 </html>
 '''
 
-
 # A single movie entry html template
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
@@ -128,40 +152,35 @@ movie_tile_content = '''
 </div>
 '''
 
-
 def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
     for movie in movies:
         # Extract the youtube ID from the url
-        youtube_id_match = re.search(
-            r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
-        youtube_id_match = youtube_id_match or re.search(
-            r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
-        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
-                              else None)
+        youtube_id_match = re.search(r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
+        youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
+        trailer_youtube_id = youtube_id_match.group(0) if youtube_id_match else None
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
             trailer_youtube_id=trailer_youtube_id
+
         )
     return content
 
-
 def open_movies_page(movies):
-    # Create or overwrite the output file
-    output_file = open('fresh_tomatoes.html', 'w')
+  # Create or overwrite the output file
+  output_file = open('fresh_tomatoes.html', 'w')
 
-    # Replace the movie tiles placeholder generated content
-    rendered_content = main_page_content.format(
-        movie_tiles=create_movie_tiles_content(movies))
+  # Replace the placeholder for the movie tiles with the actual dynamically generated content
+  rendered_content = main_page_content.format(movie_tiles=create_movie_tiles_content(movies))
 
-    # Output the file
-    output_file.write(main_page_head + rendered_content)
-    output_file.close()
+  # Output the file
+  output_file.write(main_page_head + rendered_content)
+  output_file.close()
 
-    # open the output file in the browser (in a new tab, if possible)
-    url = os.path.abspath(output_file.name)
-    webbrowser.open('file://' + url, new=2)
+  # open the output file in the browser
+  url = os.path.abspath(output_file.name)
+  webbrowser.open('file://' + url, new=2) # open in a new tab, if possible
