b45fd4f3cc63ef9eeed8deb7054983b9e4d51f0c
Eduardo Mendivil <emendivil@sci.utah.edu>
2017-10-23 22:07:16 -0600

    change on media.py

--- media.py
+++ media.py
@@ -1,7 +1,8 @@
 import webbrowser # import the webbrowser module to use its open() method
 
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube): # the __init__ function initializes the class
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube): # the __init__ function initializes the class
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
